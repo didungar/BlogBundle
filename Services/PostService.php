6568b34425e280cee879f6db60d26f143ce9e900
Didier Ungar <didungar@gmail.com>
2016-05-07 19:28:15 +0200

    Ajout du filtre par catégorie dans le service

--- Services/PostService.php
+++ Services/PostService.php
@@ -16,17 +16,44 @@ class PostService {
 		);
 	}
 	public function getPosts(array $aArgs = []) {
+		// Initialisation :
 		$iFirstResult = 0;
 		$iMaxResults = 10;
+		$aWhere = [];
+		$aParameters = [];
 		$em = $this->container->get('doctrine')->getManager();
+
+		// Analyse de la demande :
+		if ( !empty($aArgs['id_categ']) ) {
+			if ( !is_array($aArgs['id_categ']) ) {
+				$aArgs['id_categ'] = [$aArgs['id_categ']];
+			}
+			foreach($aArgs['id_categ'] as $id_categ) {
+				if ( ! is_numeric($id_categ) ) {
+					throw new \Exception('id_categ bad value (not array / numeric)');
+				}
+				if ( $id_categ<=0 ) {
+					throw new \Exception('id_categ not found : '.$id_categ);
+				}
+			}
+			$aWhere[] = 'p.idCateg IN(:id_categ)';
+			$aParameters[':id_categ'] = $aArgs['id_categ'];
+		}
+
+		// Query :
+		$sWhere = empty($aWhere) ? '' : ' WHERE '.implode(' AND ', $aWhere);
 		$query = $em->createQuery(
-			    'SELECT p
-			    FROM DidUngarBlogBundle:Post p
-			    ORDER BY p.dateAdd DESC'
+			'SELECT p
+			FROM DidUngarBlogBundle:Post p
+			'.$sWhere.'
+			ORDER BY p.dateAdd DESC'
 			)
 			->setFirstResult($iFirstResult)
                 	->setMaxResults($iMaxResults);
-
+		if ( ! empty($aParameters) ) {
+			$query->setParameters($aParameters);
+		}
+		// Résultat :
 		return $query->getResult();//$em->getRepository('DidUngarBlogBundle:Post')->findBy([],['dateAdd'=>'DESC']);
 	}
 }
