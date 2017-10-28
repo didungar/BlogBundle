ec18007026bd1934b194d48f0c5671e828c08682
Didier Ungar <didier.ungar@gmail.com>
2017-10-28 20:19:37 +0200

    indentation propre

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -9,91 +9,92 @@ use DidUngar\BlogBundle\Services\PostService;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/articles/{id_post}-{slug}.html", name="DidBlog_Article")
-     * @Template()
-     */
-    public function getPostAction($id_post, $slug)
-    {
-	$oPostService = new PostService($this->get('service_container'));
-	$oPost = $oPostService->getPost($id_post);
-	$aData = ['oPost'=>$oPost,];
-	if ( $this->getParameter('DidUngarBlogBundle_DateService') ) {
-		$sDateService = $this->getParameter('DidUngarBlogBundle_DateService');
-		$aData['oDateService'] = new $sDateService($this->get('service_container'));
-	}
-	if ( $this->getParameter('DidUngarBlogBundle_getPostTpl') ) {
-		return $this->render($this->getParameter('DidUngarBlogBundle_getPostTpl'), $aData);
-	}
-        return $aData;
-    }
-
-    /**
-     * @Route("/articles", name="DidBlog_Articles")
-     * @Route("/articles/")
-     * @Template()
-     */
-    public function getPostsAction()
-    {
-	$oPostService = new PostService($this->get('service_container'));
-	$aPosts = $oPostService->getPosts();
-	$aData = ['aPosts'=>$aPosts,];
-	if ( $this->getParameter('DidUngarBlogBundle_DateService') ) {
-		$sDateService = $this->getParameter('DidUngarBlogBundle_DateService');
-		$aData['oDateService'] = new $sDateService($this->get('service_container'));
-	}
-	if ( $this->getParameter('DidUngarBlogBundle_getPostsTpl') ) {
-		return $this->render($this->getParameter('DidUngarBlogBundle_getPostsTpl'), $aData);
-	}
-        return $aData;
-    }
-
-    /**
-     * @Route("/articles-{slug}", name="DidBlog_ArticlesOfCateg")
-     * @Route("/articles-{slug}/")
-     * @Template()
-     */
-    public function getPostsOfCategAction($slug)
-    {
-	$em = $this->get('service_container')->get('doctrine')->getManager();
-	$oCateg = $em->getRepository('DidUngarBlogBundle:Categ')->findOneBy(
-		['slug'=>$slug,]
-	);
-	if ( empty($oCateg) ) {
-		throw new \Exception('$oCateg not found');
+	/**
+	 * @Route("/articles/{id_post}-{slug}.html", name="DidBlog_Article")
+	 * @Template()
+	 */
+	public function getPostAction($id_post, $slug)
+	{
+		$oPostService = new PostService($this->get('service_container'));
+		$oPost = $oPostService->getPost($id_post);
+		$aData = ['oPost' => $oPost,];
+		if ($this->getParameter('DidUngarBlogBundle_DateService')) {
+			$sDateService = $this->getParameter('DidUngarBlogBundle_DateService');
+			$aData['oDateService'] = new $sDateService($this->get('service_container'));
+		}
+		if ($this->getParameter('DidUngarBlogBundle_getPostTpl')) {
+			return $this->render($this->getParameter('DidUngarBlogBundle_getPostTpl'), $aData);
+		}
+		return $aData;
 	}
 
-	$oPostService = new PostService($this->get('service_container'));
-	$aPosts = $oPostService->getPosts(['id_categ'=>$oCateg->getId(),]);
-	$aData = ['aPosts'=>$aPosts,];
-	if ( $this->getParameter('DidUngarBlogBundle_DateService') ) {
-		$sDateService = $this->getParameter('DidUngarBlogBundle_DateService');
-		$aData['oDateService'] = new $sDateService($this->get('service_container'));
+	/**
+	 * @Route("/articles", name="DidBlog_Articles")
+	 * @Route("/articles/")
+	 * @Template()
+	 */
+	public function getPostsAction()
+	{
+		$oPostService = new PostService($this->get('service_container'));
+		$aPosts = $oPostService->getPosts();
+		$aData = ['aPosts' => $aPosts,];
+		if ($this->getParameter('DidUngarBlogBundle_DateService')) {
+			$sDateService = $this->getParameter('DidUngarBlogBundle_DateService');
+			$aData['oDateService'] = new $sDateService($this->get('service_container'));
+		}
+		if ($this->getParameter('DidUngarBlogBundle_getPostsTpl')) {
+			return $this->render($this->getParameter('DidUngarBlogBundle_getPostsTpl'), $aData);
+		}
+		return $aData;
 	}
-	if ( $this->getParameter('DidUngarBlogBundle_getPostsTpl') ) {
-		return $this->render($this->getParameter('DidUngarBlogBundle_getPostsTpl'), $aData);
+
+	/**
+	 * @Route("/articles-{slug}", name="DidBlog_ArticlesOfCateg")
+	 * @Route("/articles-{slug}/")
+	 * @Template()
+	 */
+	public function getPostsOfCategAction($slug)
+	{
+		$em = $this->get('service_container')->get('doctrine')->getManager();
+		$oCateg = $em->getRepository('DidUngarBlogBundle:Categ')->findOneBy(
+			['slug' => $slug,]
+		);
+		if (empty($oCateg)) {
+			throw new \Exception('$oCateg not found');
+		}
+
+		$oPostService = new PostService($this->get('service_container'));
+		$aPosts = $oPostService->getPosts(['id_categ' => $oCateg->getId(),]);
+		$aData = ['aPosts' => $aPosts,];
+		if ($this->getParameter('DidUngarBlogBundle_DateService')) {
+			$sDateService = $this->getParameter('DidUngarBlogBundle_DateService');
+			$aData['oDateService'] = new $sDateService($this->get('service_container'));
+		}
+		if ($this->getParameter('DidUngarBlogBundle_getPostsTpl')) {
+			return $this->render($this->getParameter('DidUngarBlogBundle_getPostsTpl'), $aData);
+		}
+		return $aData;
 	}
-        return $aData;
-    }
 
-    /**
-     * @Route("/author/{id_author}.html")
-     * @Template()
-     */
-    public function getAuthorAction($id_author)
-    {
-	$em = $this->getDoctrine()->getManager();
-	$repAuthors = $em->getRepository('DidUngarBlogBundle:Author');
-	$oAuthor = $repAuthors->find($id_author);
+	/**
+	 * @Route("/author/{id_author}.html")
+	 * @Template()
+	 */
+	public function getAuthorAction($id_author)
+	{
+		$em = $this->getDoctrine()->getManager();
+		$repAuthors = $em->getRepository('DidUngarBlogBundle:Author');
+		$oAuthor = $repAuthors->find($id_author);
 
-        return ['oAuthor'=>$oAuthor,];
-    }
-    /**
-     * @Route("/author/{id_post}-{name}.html")
-     * @Template()
-     */
-    public function getAuthorNameAction($id_author, $name)
-    {
-        return $this->getAuthorAction($id_author);
-    }
+		return ['oAuthor' => $oAuthor,];
+	}
+
+	/**
+	 * @Route("/author/{id_post}-{name}.html")
+	 * @Template()
+	 */
+	public function getAuthorNameAction($id_author, $name)
+	{
+		return $this->getAuthorAction($id_author);
+	}
 }
