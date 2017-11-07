63b475e11bebc3a3a252c9b6e8a167cf8e3b5aa6
Didier Ungar <didier.ungar@gmail.com>
2017-11-07 18:02:19 +0100

    code style

--- Entity/Categ.php
+++ Entity/Categ.php
@@ -12,180 +12,180 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Categ
 {
-    /**
-     * @var integer
-     *
-     * @ORM\Column(name="id", type="integer")
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="AUTO")
-     */
-    private $id;
+	/**
+	 * @var integer
+	 *
+	 * @ORM\Column(name="id", type="integer")
+	 * @ORM\Id
+	 * @ORM\GeneratedValue(strategy="AUTO")
+	 */
+	private $id;
 
-    /**
-     * @var integer
-     *
-     * @ORM\Column(name="id_blog", type="integer")
-     */
-    private $idBlog;
+	/**
+	 * @var integer
+	 *
+	 * @ORM\Column(name="id_blog", type="integer")
+	 */
+	private $idBlog;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="slug", type="string", length=35)
-     */
-    private $slug;
+	/**
+	 * @var string
+	 *
+	 * @ORM\Column(name="slug", type="string", length=35)
+	 */
+	private $slug;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="title", type="string", length=35)
-     */
-    private $title;
+	/**
+	 * @var string
+	 *
+	 * @ORM\Column(name="title", type="string", length=35)
+	 */
+	private $title;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="order_cols", type="string", length=30)
-     */
-    private $orderCols;
+	/**
+	 * @var string
+	 *
+	 * @ORM\Column(name="order_cols", type="string", length=30)
+	 */
+	private $orderCols;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="order_sens", type="string", length=5)
-     */
-    private $orderSens;
-
-
-    /**
-     * Get id
-     *
-     * @return integer
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * Set idBlog
-     *
-     * @param integer $idBlog
-     *
-     * @return Categ
-     */
-    public function setIdBlog($idBlog)
-    {
-        $this->idBlog = $idBlog;
-
-        return $this;
-    }
-
-    /**
-     * Get idBlog
-     *
-     * @return integer
-     */
-    public function getIdBlog()
-    {
-        return $this->idBlog;
-    }
-
-    /**
-     * Set slug
-     *
-     * @param string $slug
-     *
-     * @return Categ
-     */
-    public function setSlug($slug)
-    {
-        $this->slug = $slug;
-
-        return $this;
-    }
-
-    /**
-     * Get slug
-     *
-     * @return string
-     */
-    public function getSlug()
-    {
-        return $this->slug;
-    }
-
-    /**
-     * Set title
-     *
-     * @param string $title
-     *
-     * @return Categ
-     */
-    public function setTitle($title)
-    {
-        $this->title = $title;
-
-        return $this;
-    }
-
-    /**
-     * Get title
-     *
-     * @return string
-     */
-    public function getTitle()
-    {
-        return $this->title;
-    }
+	/**
+	 * @var string
+	 *
+	 * @ORM\Column(name="order_sens", type="string", length=5)
+	 */
+	private $orderSens;
 
 
-    /**
-     * Set order_cols
-     *
-     * @param string $orderCols
-     *
-     * @return Categ
-     */
-    public function setOrderCols($orderCols)
-    {
-        $this->orderCols = $orderCols;
+	/**
+	 * Get id
+	 *
+	 * @return integer
+	 */
+	public function getId()
+	{
+		return $this->id;
+	}
 
-        return $this;
-    }
+	/**
+	 * Set idBlog
+	 *
+	 * @param integer $idBlog
+	 *
+	 * @return Categ
+	 */
+	public function setIdBlog($idBlog)
+	{
+		$this->idBlog = $idBlog;
 
-    /**
-     * Get orderCols
-     *
-     * @return string
-     */
-    public function getOrderCols()
-    {
-        return $this->orderCols;
-    }
+		return $this;
+	}
 
-    /**
-     * Set order_sens
-     *
-     * @param string $orderSens
-     *
-     * @return Categ
-     */
-    public function setOrderSens($orderSens)
-    {
-        $this->orderSens = $orderSens;
+	/**
+	 * Get idBlog
+	 *
+	 * @return integer
+	 */
+	public function getIdBlog()
+	{
+		return $this->idBlog;
+	}
 
-        return $this;
-    }
+	/**
+	 * Set slug
+	 *
+	 * @param string $slug
+	 *
+	 * @return Categ
+	 */
+	public function setSlug($slug)
+	{
+		$this->slug = $slug;
 
-    /**
-     * Get orderSens
-     *
-     * @return string
-     */
-    public function getOrderSens()
-    {
-        return $this->orderSens;
-    }
+		return $this;
+	}
+
+	/**
+	 * Get slug
+	 *
+	 * @return string
+	 */
+	public function getSlug()
+	{
+		return $this->slug;
+	}
+
+	/**
+	 * Set title
+	 *
+	 * @param string $title
+	 *
+	 * @return Categ
+	 */
+	public function setTitle($title)
+	{
+		$this->title = $title;
+
+		return $this;
+	}
+
+	/**
+	 * Get title
+	 *
+	 * @return string
+	 */
+	public function getTitle()
+	{
+		return $this->title;
+	}
+
+
+	/**
+	 * Set order_cols
+	 *
+	 * @param string $orderCols
+	 *
+	 * @return Categ
+	 */
+	public function setOrderCols($orderCols)
+	{
+		$this->orderCols = $orderCols;
+
+		return $this;
+	}
+
+	/**
+	 * Get orderCols
+	 *
+	 * @return string
+	 */
+	public function getOrderCols()
+	{
+		return $this->orderCols;
+	}
+
+	/**
+	 * Set order_sens
+	 *
+	 * @param string $orderSens
+	 *
+	 * @return Categ
+	 */
+	public function setOrderSens($orderSens)
+	{
+		$this->orderSens = $orderSens;
+
+		return $this;
+	}
+
+	/**
+	 * Get orderSens
+	 *
+	 * @return string
+	 */
+	public function getOrderSens()
+	{
+		return $this->orderSens;
+	}
 }
 
--- Services/PostService.php
+++ Services/PostService.php
@@ -1,28 +1,36 @@
 <?php
+
 namespace DidUngar\BlogBundle\Services;
 
-class PostService {
+class PostService
+{
 	protected $container;
-	public function __construct($service_container) {
+
+	public function __construct($service_container)
+	{
 		$this->container = $service_container;
 	}
-	public function getPost($id_post) {
-		if ( ! is_numeric($id_post) ) {
+
+	public function getPost($id_post)
+	{
+		if (!is_numeric($id_post)) {
 			throw new \Exception('$id_post not a number');
 		}
 		$em = $this->container->get('doctrine')->getManager();
+
 		return $em->getRepository('DidUngarBlogBundle:Post')->findOneBy(
-			['id'=>$id_post]
+			['id' => $id_post]
 		);
 	}
 
 	/**
 	 * @param array $aArgs = []
-	 *	- array|integer id_categ optional
-	 *	- integer max_result optional
+	 *    - array|integer id_categ optional
+	 *    - integer max_result optional
 	 * @return lstPost
-	**/
-	public function getPosts(array $aArgs = []) {
+	 **/
+	public function getPosts(array $aArgs = [])
+	{
 		// Initialisation :
 		$iFirstResult = 0;
 		$iMaxResults = 10;
@@ -32,49 +40,50 @@ class PostService {
 		$sOrder = ' ORDER BY p.dateAdd DESC';
 
 		// Analyse de la demande :
-		if ( !empty($aArgs['max_result']) ) {
-			if ( ! is_numeric($aArgs['max_result']) ) {
+		if (!empty($aArgs['max_result'])) {
+			if (!is_numeric($aArgs['max_result'])) {
 				throw new \Exception('max_result bad value (not numeric)');
 			}
-			if ( $aArgs['max_result']<=0 ) {
+			if ($aArgs['max_result'] <= 0) {
 				throw new \Exception('max_result bad-value : negatif');
 			}
 			$iMaxResults = (int)$aArgs['max_result'];
 		}
-		if ( !empty($aArgs['id_categ']) ) {
-			if ( !is_array($aArgs['id_categ']) ) {
+		if (!empty($aArgs['id_categ'])) {
+			if (!is_array($aArgs['id_categ'])) {
 				$aArgs['id_categ'] = [$aArgs['id_categ']];
 			}
-			foreach($aArgs['id_categ'] as $id_categ) {
-				if ( ! is_numeric($id_categ) ) {
+			foreach ($aArgs['id_categ'] as $id_categ) {
+				if (!is_numeric($id_categ)) {
 					throw new \Exception('id_categ bad value (not array / numeric)');
 				}
-				if ( $id_categ<=0 ) {
-					throw new \Exception('id_categ not positive : '.$id_categ);
+				if ($id_categ <= 0) {
+					throw new \Exception('id_categ not positive : ' . $id_categ);
 				}
-				$oCateg = $em->getRepository('DidUngarBlogBundle:Categ')->findOneBy(['id'=>$aArgs['id_categ']]);
-				if ( empty($oCateg) ) {
-					throw new \Exception('id_categ not found : '.$id_categ);
+				$oCateg = $em->getRepository('DidUngarBlogBundle:Categ')->findOneBy(['id' => $aArgs['id_categ']]);
+				if (empty($oCateg)) {
+					throw new \Exception('id_categ not found : ' . $id_categ);
 				}
 			}
 			$aWhere[] = 'p.idCateg IN(:id_categ)';
 			$aParameters[':id_categ'] = $aArgs['id_categ'];
-			$sOrder = 'ORDER BY p.'.$oCateg->getOrderCols().' '.$oCateg->getOrderSens();
+			$sOrder = 'ORDER BY p.' . $oCateg->getOrderCols() . ' ' . $oCateg->getOrderSens();
 		}
 
 		// Query :
-		$sWhere = empty($aWhere) ? '' : ' WHERE '.implode(' AND ', $aWhere);
+		$sWhere = empty($aWhere) ? '' : ' WHERE ' . implode(' AND ', $aWhere);
 		$query = $em->createQuery(
 			'SELECT p
 			FROM DidUngarBlogBundle:Post p
-			'.$sWhere
-			.$sOrder
-			)
+			' . $sWhere
+			. $sOrder
+		)
 			->setFirstResult($iFirstResult)
-                	->setMaxResults($iMaxResults);
-		if ( ! empty($aParameters) ) {
+			->setMaxResults($iMaxResults);
+		if (!empty($aParameters)) {
 			$query->setParameters($aParameters);
 		}
+
 		// Résultat :
 		return $query->getResult();//$em->getRepository('DidUngarBlogBundle:Post')->findBy([],['dateAdd'=>'DESC']);
 	}
